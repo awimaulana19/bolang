98b97b77e023bc985a4c457c26898d6fad1aa20b
Awi Maulana <awimaulana19@gmail.com>
2023-01-19 13:22:27 +0800

    Home Hero

--- resources/views/pengguna/home.blade.php
+++ resources/views/pengguna/home.blade.php
@@ -1,58 +1,128 @@
-@extends('template.temp')
+<!doctype html>
+<html lang="en">
 
-@section('title', 'Bolang | Home')
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bolang | Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('image/icon.png') }}" />
+</head>
 
-@section('content')
-    <h1>Coba</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum reiciendis, totam vitae voluptate aut quibusdam
-        libero molestiae dignissimos quaerat culpa porro est hic necessitatibus rem a fugiat nam ut qui!</p>
-    <div class="row">
-        <div class="col-6">
-            <h1>dfd</h1>
+<body>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="{{ asset('image/logo.png') }}" alt="" width="120px">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Promo</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Lapangan</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">E-Ticket</a>
+                    </li>
+                </ul>
+                <a href="#" class="btn-temp">REGISTER</a>
+                <a href="#" class="btn-temp">LOGIN</a>
+            </div>
         </div>
-        <div class="col-6">
-            <h1>ddf</h1>
+    </nav>
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row baris-temp">
+                <div class="col-5 kata">
+                    <h1>Selamat Beraktivitas</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi suscipit non quisquam eaque
+                        eligendi,
+                        labore dolores sed. Omnis, sequi. Molestiae ipsa iure nam illum repellendus accusantium,
+                        nulla
+                        libero adipisci minima Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
+                        dolorum neque, quisquam aliquid magnam amet dignissimos eos itaque. Quidem, fuga incidunt.
+                        Incidunt, itaque a. Eveniet excepturi facilis asperiores blanditiis accusantium?</p>
+                </div>
+                <div class="col-7">
+                    <div class="d-flex justify-content-end">
+                        <img src="{{ asset('image/hero.png') }}" alt="Hero" width="400px">
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-@endsection
+    <div class="container semuakotak">
+        <div class="row">
+            <div class="col-4 kotak kotak1">
+                <a href="#"><img src="{{ asset('image/lapangan.png') }}" width="50px"></a>
+                <a href="#">Lapangan</a>
+            </div>
+            <div class="col-4 kotak">
+                <a href="#"><img src="{{ asset('image/eticket.png') }}" width="50px"></a>
+                <a href="#">E-Ticket</a>
+            </div>
+            <div class="col-4 kotak">
+                <a href="#"><img src="{{ asset('image/promo.png') }}" width="50px"></a>
+                <a href="#">Promo</a>
+            </div>
+        </div>
+        <div class="row">
+            <br>
+            <br>
+            <br>
+            <br>
+            <h1>Search</h1>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+        </div>
+        <hr>
+    </div>
+    <footer class="footer-edit">
+        <div class="container">
+            <div class="row m-auto">
+                <div class="col-3">
+                    <p class="pilihan">Menu</p>
+                    <p><a href="#">Home</a></p>
+                    <p><a href="#">E-Ticket</a></p>
+                </div>
+                <div class="col-3">
+                    <p class="pilihan">Produk</p>
+                    <p><a href="#">Promo</a></p>
+                    <p><a href="#">Lapangan</a></p>
+                </div>
+                <div class="col-3">
+                    <p class="pilihan">Bantuan</p>
+                    <p><a href="#">Help Center</a></p>
+                    <p><a href="#">Live Chat</a></p>
+                </div>
+                <div class="col-3">
+                    <p class="pilihan">Pembayaran</p>
+                    <img src="{{ asset('image/pembayaran.png') }}" alt="Pembayaran" width="200px">
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
