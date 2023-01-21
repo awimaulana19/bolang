ff9b5a5fce81445af877d430615a551ac20846d0
Syahrul273 <60900120042@uin-alauddin.ac.id>
2023-01-21 18:50:58 +0800

    Regist dan Login

--- resources/views/pengguna/login.blade.php
+++ resources/views/pengguna/login.blade.php
@@ -3,5 +3,23 @@
 @section('title', 'Bolang | Login')
 
 @section('content')
-    
+    <link rel="stylesheet" href="{{ asset('css/regist.css') }}">
+        <div class="container-fluid">
+                <div class="container">
+                    <div class="row baris-temp mt-5 mb-5">
+                        <div class="col-6 p-3">
+                            <div class="d-flex justify-content-end">
+                                <img src="{{ asset('image/regis1.png') }}" alt="regis1" width="550px">
+                            </div>
+                        </div>
+                        <div class="regist col-5 kata p-5 mx-auto">
+                            <input class="form mb-3 w-100" type="email" value="" placeholder="Username/Email">
+                            <input class="form mb-3 w-100" type="password" value="" placeholder="Password">
+                            <input class="button-primary mb-3 w-100" type="submit" value="Login">
+                            <p style="text-align: center;">Belum Punya Akun? &nbsp<a href="/registrasi">REGISTRASI DI SINI</a></p>
+                        </div>
+                        
+                    </div>
+                </div>
+        </div>
 @endsection
--- resources/views/pengguna/regist.blade.php
+++ resources/views/pengguna/regist.blade.php
@@ -3,5 +3,26 @@
 @section('title', 'Bolang | Registrasi')
 
 @section('content')
-    
+    <link rel="stylesheet" href="{{ asset('css/regist.css') }}">
+    <div class="container-fluid">
+            <div class="container">
+                <div class="row baris-temp mt-5 mb-5">
+                    <div class="col-6 p-3">
+                        <div class="d-flex justify-content-end">
+                            <img src="{{ asset('image/regis1.png') }}" alt="regis1" width="550px">
+                        </div>
+                    </div>
+                    <div class="regist col-5 kata p-2 mx-auto">
+                        <input class="form mb-3 w-100" type="text" value="" placeholder="Nama Lengkap">
+                        <input class="form mb-3 w-100" type="text" value="" placeholder="Username">
+                        <input class="form mb-3 w-100" type="email" value="" placeholder="Alamat Email">
+                        <input class="form mb-3 w-100" type="password" value="" placeholder="Masukkan Password">
+                        <input class="form mb-3 w-100" type="password" value="" placeholder="Masukkan Ulang Password">
+                        <input class="button-primary mb-3 w-100" type="submit" value="Registrasi">
+                        <p style="text-align: center;">Sudah Punya Akun? &nbsp<a href="/login">LOGIN</a></p>
+                    </div>
+                    
+                </div>
+            </div>
+    </div>
 @endsection
