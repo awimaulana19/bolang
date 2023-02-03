ed9161ccafb27cb1ebd8bd4ceec7bc67889af719
Syahrul273 <60900120042@uin-alauddin.ac.id>
2023-02-03 18:02:37 +0800

    Perbaikan Akun

--- resources/views/pengguna/akun.blade.php
+++ resources/views/pengguna/akun.blade.php
@@ -8,7 +8,7 @@
         <div class="garis col-4 my-5">
             <div class="profile">
                 <img src="{{ asset('image/user1.png') }}" alt="regis1" width="120px">
-                <p>Amel Krisandaresta <br>@krisandaresta</p>
+                <p>Amel Krisandaresta <br> <span>@krisandaresta</span></p>
                 <p class="update">Update Photo Profile</p>
                 <div class="dropzone" draggable="true">
                     <p>Drop files here to upload <br> or</p>
@@ -20,14 +20,34 @@
             <div class="list">
                 <h3>Profile</h3>
                 <p>Lengkapi Profile Anda</p>
-                <div class="mt-5">
-                    <p>Nama Lengkap: <br><input type="text" class="kolom"></p>
-                    <p>Jenis Kelamin: <br><select class="pilih" name="jk" id="">
-                        <option selected>Pilih Jenis Kelamin</option>
-                        <option value="laki-laki">Laki - laki</option>
-                        <option value="perempuan">Perempuan</option>
-                    </select></p>
-                    <p>Bulan Lahir: <br><select class="pilih" id="birthmonth" name="birthmonth">
+                <div class="row">
+                    <div class="col-6 mb-4">
+                        <label for="nama" class="form-label">Nama Lengkap:</label>
+                        <input type="text" class="form-control" id="nama" name="nama">
+                    </div>
+                    <div class="col-6 mb-4">
+                        <label for="username" class="form-label">Username:</label>
+                        <input type="text" class="form-control" id="username" name="username">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6 mb-4">
+                        <label for="jk" class="form-label">Jenis Kelamin:</label>
+                        <select class="form-select" aria-label="Default select example">
+                            <option selected>Pilih Jenis Kelamin</option>
+                            <option value="1">Laki - laki</option>
+                            <option value="2">Perempuan</option>
+                        </select>
+                    </div>
+                    <div class="col-6 mb-4">
+                        <label for="phoneNumber" class="form-label">No. Hp:</label>
+                        <input type="tel" class="form-control" id="phoneNumber" aria-describedby="phoneNumberHelp" value="+62">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6 mb-4">
+                        <label for="bulan" class="form-label">Bulan Lahir:</label>
+                        <select class="form-select" aria-label="Default select example">
                         <option value="01">Januari</option>
                         <option value="02">Februari</option>
                         <option value="03">Maret</option>
@@ -40,56 +60,61 @@
                         <option value="10">Oktober</option>
                         <option value="11">November</option>
                         <option value="12" selected>Desember</option>
-                    </select></p>
-                </div>
-                <div class="form-2">
-                    <p>Username: <br><input type="text" class="kolom"></p>
-                    <p>No Hp: <br><input type="number" class="kolom"></p>
-                    <p>Tahun Lahir: <span>Tanggal Lahir: <br><input type="text" class="thn2"></span> <input type="text" class="thn"></p>
+                        </select>
+                    </div>
+                    <div class="col-3 mb-4">
+                        <label for="tahun" class="form-label">Tahun Lahir:</label>
+                        <input type="text" class="form-control tahun" id="tahun" name="tahun">
+                    </div>
+                    <div class="col-3 mb-4">
+                        <label for="tanggal" class="form-label">Tanggal Lahir:</label>
+                        <input type="text" class="form-control tahun" id="tanggal" name="tanggal">
+                    </div>
                 </div>
                 <h6>Olahraga Favorite:</h6>
                 <div class="row favorite">
                     <div class="col-2 gambar">
-                        <img src="{{ asset('image/bola.png') }}" width="70px">
-                        <p>Sepak Bola</p>
+                        <img src="{{ asset('image/bola.png') }}" width="75px">
+                        <label class="form-label icon">Sepak Bola</label>
                     </div>
                     <div class="col-2 gambar">
-                        <img src="{{ asset('image/mini soccer.png') }}" width="70px">
-                        <p>Mini Soccer</p>
+                        <img src="{{ asset('image/mini soccer.png') }}" width="75px">
+                        <label class="form-label icon">Mini Soccer</label>
                     </div>
                     <div class="col-2 gambar">
-                        <img src="{{ asset('image/futsal2.png') }}" width="70px">
-                        <p>Futsal</p>
+                        <img src="{{ asset('image/futsal2.png') }}" width="75px">
+                        <label class="form-label icon">Futsal</label>
                     </div>
                     <div class="col-2 gambar">
-                        <img src="{{ asset('image/bultang.png') }}" width="70px">
-                        <p>Bulu Tangkis</p>
+                        <img src="{{ asset('image/bultang.png') }}" width="75px">
+                        <label class="form-label icon">BuluTangkis</label>
                     </div>
                     <div class="col-2">
-                        <img src="{{ asset('image/basket2.png') }}" width="70px">
-                        <p>Bola Basket</p>
+                        <img src="{{ asset('image/basket2.png') }}" width="75px">
+                        <label class="form-label icon">Bola Basket</label>
                     </div>
                     <div class="col-2 gambar">
-                        <img src="{{ asset('image/voli.png') }}" width="70px">
-                        <p>Bola Voli</p>
+                        <img src="{{ asset('image/voli.png') }}" width="75px">
+                        <label class="form-label icon">Bola Voli</label>
                     </div>
                     <div class="col-2 gambar">
-                        <img src="{{ asset('image/fitnes.png') }}" width="70px">
-                        <p>Fitness</p>
+                        <img src="{{ asset('image/fitnes.png') }}" width="75px">
+                        <label class="form-label icon">Fitness</label>
                     </div>
                     <div class="col-2 gambar">
-                        <img src="{{ asset('image/tenis2.png') }}" width="70px">
-                        <p>Tennis</p>
+                        <img src="{{ asset('image/tenis2.png') }}" width="75px">
+                        <label class="form-label icon">Tennis</label>
                     </div>
                     <div class="col-2 gambar">
-                        <img src="{{ asset('image/baseball.png') }}" width="70px">
-                        <p>Baseball</p>
+                        <img src="{{ asset('image/baseball.png') }}" width="75px">
+                        <label class="form-label icon">Baseball</label>
                     </div>
                     <div class="col-2">
-                        <img src="{{ asset('image/lainnya.png') }}" width="70px">
-                        <p>Olahraga Lainnya</p>
+                        <img src="{{ asset('image/lainnya.png') }}" width="75px">
+                        <label class="form-label icon">Olahraga Lainnya</label>
                     </div>
                 </div>
+                <button type="submit" class="btn btn-primary">SIMPAN PROFIL</button>
             </div>
         </div>
     </div>
